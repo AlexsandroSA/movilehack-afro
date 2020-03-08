9112428581a7eac71f46a30a7c0fee00d504f583
Dan Jesus <danper.jesus@gmail.com>
2020-03-07 23:22:20 -0300

    Add base api

--- api/index.js
+++ api/index.js
@@ -1,1 +1,24 @@
-const express = require('express');
+"use strict"
+
+const Hapi = require("@hapi/hapi")
+
+const init = async () => {
+  const server = Hapi.server({
+    port: process.env.PORT || 3000,
+    host: process.env.HOST || "localhost"
+  })
+
+  server.route({
+    method: "GET",
+    path: "/",
+    handler: function(req, h) {
+      return "Movile Hack Afro - Xpert"
+    }
+  })
+
+  await server.start();
+
+  console.info("Server running on %s", server.info.uri)
+}
+
+init()
