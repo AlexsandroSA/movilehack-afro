2cf4863c6e8fc52180e192e6ad8a7025dc2fcbfa
Alexsandro Silva Alves <alexsandro-sa@MacBook-Pro-de-Alexsandro.local>
2020-03-07 23:47:37 -0300

    feat: add page Student and Teacher

---
+++ front/src/pages/Teacher/Teacher.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+
+const Teacher: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Home professor</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Teacher;
