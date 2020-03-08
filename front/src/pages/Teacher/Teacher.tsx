ec3e80069e250dc7f695ebe2d5fb3ee3c2fc453a
Dan Jesus <danper.jesus@gmail.com>
2020-03-08 01:38:08 -0300

    Base student

--- front/src/pages/Teacher/Teacher.tsx
+++ front/src/pages/Teacher/Teacher.tsx
@@ -1,15 +1,47 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import {
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonToggle,
+  IonRadio,
+  IonCheckbox,
+  IonItemSliding,
+  IonItemOption,
+  IonItemOptions,
+  IonContent
+} from "@ionic/react";
+
+const classes = [
+  { name: "6 Ano A" },
+  { name: "7 Ano A" },
+  { name: "8 Ano A" },
+  { name: "9 Ano A" },
+  { name: "5 Ano A" },
+  { name: "4 Ano A" }
+];
 
 const Teacher: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Home professor</IonTitle>
-        </IonToolbar>
-      </IonHeader>
+
       <IonContent>
+        <IonList>
+          {classes.map(classe => (
+            <IonItem
+              button
+              key={Math.random()}
+              routerLink="/professor/classe"
+            >
+              <IonLabel>{classe.name}</IonLabel>
+            </IonItem>
+          ))}
+        </IonList>
       </IonContent>
     </IonPage>
   );
